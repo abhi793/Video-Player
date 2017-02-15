615087cd37199779260789c9e58bd2ec5cab1688
abhi793 <abhichaudhary793@gmail.com>
2017-02-16 03:43:51 +0530

    Create indicator view

--- app/src/main/java/com/example/abhi/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/abhi/videoplayer/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.abhi.videoplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.abhi.videoplayer.uicomponents.IndicatorView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView inTheatresListView;
+    private RecyclerView upcomingListView;
+
+    private IndicatorView indicatorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        indicatorView.setTotalItems(5);
+        indicatorView.setIndicatorPosition(1);
+    }
+
+    public void initializeViews() {
+        inTheatresListView = (RecyclerView) findViewById(R.id.in_theatres);
+        upcomingListView = (RecyclerView) findViewById(R.id.upcoming);
+
+        indicatorView = (IndicatorView) findViewById(R.id.in_theatres_indicator);
     }
 }
---
+++ app/src/main/java/com/example/abhi/videoplayer/adapter/ListModel.java
@@ -0,0 +1,26 @@
+package com.example.abhi.videoplayer.adapter;
+
+/**
+ * Created by abhic on 16-02-2017.
+ */
+
+public class ListModel {
+    private int imageRes;
+    private String text;
+
+    public int getImageRes() {
+        return imageRes;
+    }
+
+    public void setImageRes(int imageRes) {
+        this.imageRes = imageRes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
